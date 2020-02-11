54b04be9d90076b8581dd855f08059c2b99752ca
Mohammd Khajah <zaiadazagaz@gmail.com>
2020-02-11 05:58:31 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,6 @@ module.exports = {
             new webpack.optimize.CommonsChunkPlugin({
                 name: 'vendor',
                 minChunks: function (module) {
-                   // this assumes your vendor imports exist in the node_modules directory
                    return module.context && module.context.indexOf('node_modules') !== -1;
                 }
             }),
